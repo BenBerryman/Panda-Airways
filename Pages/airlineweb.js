23ed06851bc7e5c7d98d41e38a2e0f3f2d7fc3ad
Benjamin Berryman <benjamin.a.berryman@gmail.com>
2020-11-19 22:11:21 -0600

    Added main flight finder without functionality yet

--- Pages/airlineweb.js
+++ Pages/airlineweb.js
@@ -11,3 +11,40 @@ setInterval(function() {
         .appendTo('#slideshow');
 }, 25000);
 
+$(document).ready(function() {
+    $('#book').show();
+    $('#status').hide();
+
+    var date = new Date();
+    var dd = ('0'+date.getDate()).slice(-2);
+    var mm = ('0'+(date.getMonth()+1)).slice(-2);
+    var yyyy = date.getFullYear();
+    today = yyyy+'-'+mm+'-'+dd;
+    date.setDate(date.getDate()+61);
+    dd = ('0'+date.getDate()).slice(-2);
+    mm = ('0'+(date.getMonth()+1)).slice(-2);
+    yyyy = date.getFullYear();
+    max = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("dates").setAttribute('min', today);
+    document.getElementById("dates").setAttribute('max', max);
+
+    $('#bookShow').click(function() {
+        $('#status').hide();
+        $('#book').show();
+        $('#idCaret').css('left','18.5%');
+
+    });
+    $('#statusShow').click(function() {
+        $('#book').hide();
+        $('#status').show();
+        $('#idCaret').css('left','51.7%');
+    });
+});
+
+function changeText() {
+    var value = document.getElementById('travelers').value;
+    if(value > 1)
+        document.getElementById('travelText').innerHTML = "travelers";
+    else
+        document.getElementById('travelText').innerHTML = "traveler";
+}
