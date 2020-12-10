3c2c779fab99bf1bb0b19fca1f3c3a02fd8b0b4b
Benjamin Berryman <benjamin.a.berryman@gmail.com>
2020-12-10 15:19:53 -0600

    shell scripts

--- Server/runserver.bat
+++ Server/runserver.bat
@@ -1,4 +1,4 @@
 
-(node index.js) &
-(echo "Web server started on port 8000"
-./node_modules/.bin/http-server ../Client -a localhost -s -p 8000)
+start node index.js
+echo "Web server started on port 8000"
+start ./node_modules/.bin/http-server ../Client -a localhost -s -p 8000
